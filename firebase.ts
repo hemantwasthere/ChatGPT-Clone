75f3e0db84d55168fe0a17aa90ca0ed5b6c09140
Hemant <hrsmart2150@gmail.com>
2023-03-10 12:37:12 +0530

    Fixed: Some minor errors & warnings

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,22 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY!,
+//   authDomain: "chatgptclone-d464e.firebaseapp.com",
+//   projectId: process.env.FIREBASE_PROJECT_ID!,
+//   storageBucket: "chatgptclone-d464e.appspot.com",
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+//   appId: process.env.FIREBASE_APP_ID!
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY!,
+  apiKey: "AIzaSyAJZWVzlnIfRLrwXabr8Kd5vq5mYqbs3D4",
   authDomain: "chatgptclone-d464e.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID!,
+  projectId: "chatgptclone-d464e",
   storageBucket: "chatgptclone-d464e.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.FIREBASE_APP_ID!
+  messagingSenderId: "195407512476",
+  appId: "1:195407512476:web:6036b3344e32252f547e59"
 };
 
 // Initialize Firebase
