ae368cf52ffc1d1b1d71298852b95904239dbdef
Hemant <hrsmart2150@gmail.com>
2023-05-19 18:43:30 +0530

    Changed firebase rules

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,92 +1,112 @@
-'use client'
+"use client";
 
-import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
-import { useSession } from 'next-auth/react'
-import { FormEvent, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import useSWR from 'swr'
-import { db } from '../firebase'
-import ModelSelection from './ModelSelection'
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { FormEvent, useState } from "react";
+import { toast } from "react-hot-toast";
+import useSWR from "swr";
+import { db } from "../firebase";
+import ModelSelection from "./ModelSelection";
 
 type Props = {
-    chatId: string;
-}
+  chatId: string;
+};
 
 function ChatInput({ chatId }: Props) {
-    const [prompt, setPropmt] = useState('')
-    const { data: session } = useSession()
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
 
-    const { data: model } = useSWR('model', {
-        fallbackData: 'text-davinci-003'
+  const { data: model } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt || prompt === "") return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name!}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+
+    const notification = toast.loading("ChatGPT is thinking...");
+
+    // Toast notification
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
     })
+      .then(() => {
+        // Toast notification for successful!
+        toast.success("ChatGPT has responded!", {
+          id: notification,
+        });
+      })
+      .catch(() => {
+        // Toast notification for error!
+        toast.error("ChatGPT has failed to respond!", {
+          id: notification,
+        });
+      });
+  };
 
-    const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        if (!prompt) return;
+  return (
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
+        <input
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here..."
+        />
+        <button
+          title={`${!prompt ? "disabled" : "press or hit enter"}`}
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+          disabled={!prompt || !session}
+          type="submit"
+        >
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
 
-        const input = prompt.trim()
-        setPropmt("")
-
-        const message: Message = {
-            text: input,
-            createdAt: serverTimestamp(),
-            user: {
-                _id: session?.user?.email!,
-                name: session?.user?.name!,
-                avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name!}`,
-            }
-        }
-
-        await addDoc(
-            collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages')
-            , message
-        )
-
-        const notification = toast.loading('ChatGPT is thinking...')
-
-        // Toast notification
-        await fetch('/api/askQuestion', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                prompt: input,
-                chatId,
-                model,
-                session
-            })
-        }).then(() => {
-            // Toast notification for successful!
-            toast.success('ChatGPT has responded!', {
-                id: notification
-            })
-        })
-    }
-
-    return (
-        <div className='bg-gray-700/50 text-gray-400 rounded-lg text-sm'>
-            <form onSubmit={sendMessage} className='p-5 space-x-5 flex' >
-                <input
-                    className='bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300'
-                    disabled={!session}
-                    value={prompt}
-                    onChange={(e) => setPropmt(e.target.value)} type="text" placeholder='Type your message here...' />
-                <button
-                    title={`${!prompt ? 'disabled' : 'press or hit enter'}`}
-                    className='bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed'
-                    disabled={!prompt || !session}
-                    type='submit'>
-                    <PaperAirplaneIcon className='h-4 w-4 -rotate-45' />
-                </button>
-            </form>
-
-            <div className='sm:hidden  ' >
-                <ModelSelection />
-            </div>
-        </div>
-    )
+      <div className="sm:hidden  ">
+        <ModelSelection />
+      </div>
+    </div>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,14 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
 
 // Before:
-// const configuration = new Configuration({
-//     apiKey: process.env.OPEN_API_KEY,
-// })
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_API_KEY,
+})
 
-const openai = new OpenAIApi(
-    new Configuration({ apiKey: process.env.OPEN_API_KEY })
-);
+// const openai = new OpenAIApi(
+//     new Configuration({ apiKey: process.env.OPEN_API_KEY })
+// );
 
-// const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration)
 
 export default openai;
